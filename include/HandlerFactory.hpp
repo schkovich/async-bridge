80d64eff473660455a83654e0a930a275954ae3e
Goran <schkovich@users.noreply.github.com>
2025-10-02 16:25:00 +0200

    chore(async-bridge: Add singleton pattern to HandlerFactory. - Refactored HandlerFactory to use a singleton pattern. - Added static initialise() and instance() methods for global access. - Updated member visibility and documentation. - Ensured HandlerFactory is initialised before use. - Improved code structure for dependency injection and testability.

--- include/HandlerFactory.hpp
+++ include/HandlerFactory.hpp
@@ -8,26 +8,49 @@
 
 namespace async_bridge {
 
-class EphemeralBridge;
+    class EphemeralBridge;
 
-/**
- * Minimal factory used to create ephemeral handlers. During migration this
- * can be replaced by a DI-friendly factory; for now a small singleton helper
- * is provided for convenience in tests and examples.
- */
-class HandlerFactory {
-public:
-    explicit HandlerFactory(IAsyncContext &ctx) : m_ctx(ctx) {}
+    /**
+     * Minimal factory used to create ephemeral handlers. During migration this
+     * can be replaced by a DI-friendly factory; for now a small singleton
+     * helper is provided for convenience in tests and examples.
+     */
+    class HandlerFactory {
+            IAsyncContext &m_ctx;
+            inline static HandlerFactory *s_instance =
+                nullptr; /**< Singleton instance pointer */
+            explicit HandlerFactory(IAsyncContext &ctx) : m_ctx(ctx) {}
 
-    template <typename HandlerType, typename... Args>
-    void run(Args &&... args) {
-        static_assert(std::is_base_of_v<EphemeralBridge, HandlerType>, "HandlerType must derive from EphemeralBridge");
-        HandlerType::create(m_ctx, std::forward<Args>(args)...);
-    }
+        public:
+            /**
+             * @brief Initializes the singleton instance
+             *
+             * Must be called once during application startup before any
+             * handlers are created.
+             *
+             * @param ctx Reference to the AsyncCtx used by all handlers
+             */
+            static void initialise(AsyncCtx &ctx) {
+                s_instance = new HandlerFactory(ctx);
+            }
 
-private:
-    IAsyncContext &m_ctx;
-};
+            /**
+             * @brief Gets the singleton instance
+             *
+             * @return Reference to the singleton HandlerFactory instance
+             * @throws assertion failure if called before initialise()
+             */
+            static HandlerFactory &instance() {
+                assert(s_instance &&
+                       "HandlerFactory must be initialised before use");
+                return *s_instance;
+            }
+            template <typename HandlerType, typename... Args>
+            void run(Args &&...args) {
+                static_assert(std::is_base_of_v<EphemeralBridge, HandlerType>,
+                              "HandlerType must derive from EphemeralBridge");
+                HandlerType::create(m_ctx, std::forward<Args>(args)...);
+            }
+    };
 
 } // namespace async_bridge
-
